0d60cacd12c754e624e85a4b2e615565745d9262
Ms-Zarina <130992803+Ms-Zarina@users.noreply.github.com>
2023-11-04 15:49:06 +0600

    Add files via upload

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,17 @@
+
+
+function Header() {
+    return (
+        <div className="header_wrapper">
+            <div className="container">
+                <div className="header">
+                    <div className="header_logo">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
